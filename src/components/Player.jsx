88798dc36f117c43f4e08f53d59d8aa4f1154461
gyubo <yoon9yubo@gmail.com>
2025-01-05 23:24:31 +0900

    0105 useRef()

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import {useState, useRef} from 'react';
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
+
+
+  // function handleChange(event) {
+  //   setEnteredPlayerNameß(event.target.value);
+  // }
+
+  function handClick() {
+    setEnteredPlayerName(playerName.current.value)
+    playerName.current.value = ''; 
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}</h2>
+      {/* <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2> */}
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref = {playerName} type="text" />
+        <button onClick={handClick}>Set Name</button>
       </p>
     </section>
   );
